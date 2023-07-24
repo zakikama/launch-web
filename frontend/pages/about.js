9fea422c1dd2d8631f37f06c15f8219049583d1d
githuberre <hibafaris123289@gmail.com>
2023-07-24 17:59:44 +0100

    techno

---
+++ frontend/pages/about.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import "../styles/about.css"
+import '../styles/fonts.css';
+
+
+function about() {
+  return (
+    <div>
+      <h1>
+        Wearable technology has become a game-changer in the world of fitness
+      </h1>
+      <div className="images">
+        <img src="/assets/orangeD.svg" alt="img" />
+        <img src="/assets/orangeD.svg" alt="img" />
+        <img src="/assets/orangeD.svg" alt="img" />
+        <img src="/assets/orangeD.svg" alt="img" />
+      </div>
+      <h1 className='p2'>The seamless integration of these devices into our daily routines, combined with their user-friendly interfaces and comfortable designs, makes them indispensable companions on our fitness journey</h1>
+    </div>
+  );
+}
+export default about
+
+
+
+  
---
+++ frontend/pages/launch.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import "../styles/launch.css"
+import '../styles/fonts.css';
+
+function launch () {
+  return (
+    <div>
+         <div className='images1'>
+        <img src="assets\Logo.svg"alt='img'></img>
+        <h1>EXPERIENCE THE LUXONIK DIFFERENCE.</h1>
+        <h1> ELEVATE YOUR FITNESS JOURNEY! </h1>
+    </div>
+
+    <div className='image2'>
+        <img src="assets\D_screen.svg"alt='img'/>
+    </div>
+    </div>
+   
+    
+        
+  )
+}
+
+export default launch
